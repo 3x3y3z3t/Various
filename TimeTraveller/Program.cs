bacea55ce97f40fe82159de917d51e875254fe79
3x3y3z3t <recette.lemongrass@yahoo.com.vn>
2024-04-30 20:56:34 +0900

    Initial commit

--- TimeTraveller/Program.cs
+++ TimeTraveller/Program.cs
@@ -11,6 +11,41 @@ namespace TimeTraveller
         //[STAThread]
         static void Main()
         {
+            //public const string SE_SYSTEMTIME_NAME = "SeSystemtimePrivilege";
+            //public const uint SE_PRIVILEGE_ENABLED = 0x00000002;
+
+            ////IntPtr handle = Process.GetCurrentProcess().Handle;
+            //IntPtr handle = Kerner32.GetCurrentProcess();
+
+            //IntPtr token;
+            //if (!AdvAPI32.OpenProcessToken(handle, (ushort)AccessToken.Query | (ushort)AccessToken.AdjustPrivileges, out token))
+            //{
+            //    Console.Write("OpenProcessToken() failed (errCode =  " + Marshal.GetLastWin32Error() + ")");
+            //    return; // TODO: don't actually return;
+            //}
+
+            //if (!AdvAPI32.LookupPrivilegeValue(null, SE_SYSTEMTIME_NAME, out LUID privilegeLuid))
+            //{
+            //    Console.Write("LookupPrivilegeValue() failed (errCode =  " + Marshal.GetLastWin32Error() + ")");
+            //    Kerner32.CloseHandle(token);
+            //    return; // TODO: don't actually return;
+            //}
+
+            //TOKEN_PRIVILEGES privilege;
+            //privilege.PrivilegeCount = 1;
+            //privilege.Luid = privilegeLuid;
+            //privilege.Attributes = SE_PRIVILEGE_ENABLED;
+
+            //if (!AdvAPI32.AdjustTokenPrivileges(token, false, ref privilege, 0U, IntPtr.Zero, IntPtr.Zero))
+            //{
+            //    Console.Write("AdjustTokenPrivileges() failed (errCode =  " + Marshal.GetLastWin32Error() + ")");
+            //    return; // TODO: don't actually return;
+            //}
+
+            //Kerner32.CloseHandle(token);
+
+            //Console.WriteLine("Privilege set.");
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
--- TimeTraveller/WinAPIImports.cs
+++ TimeTraveller/WinAPIImports.cs
@@ -4,6 +4,12 @@ using System.Runtime.InteropServices;
 
 namespace exw
 {
+    //public enum AccessToken : ushort
+    //{
+    //    Query = 0x0008,
+    //    AdjustPrivileges = 0x0020,
+    //}
+
     [StructLayout(LayoutKind.Sequential)]
     internal struct SYSTEMTIME
     {
@@ -34,9 +40,31 @@ namespace exw
         }
     }
 
+    //[StructLayout(LayoutKind.Sequential)]
+    //public struct LUID
+    //{
+    //    public UInt32 LowPart;
+    //    public Int32 HighPart;
+    //}
+
+    //[StructLayout(LayoutKind.Sequential)]
+    //public struct TOKEN_PRIVILEGES
+    //{
+    //    public UInt32 PrivilegeCount;
+    //    public LUID Luid;
+    //    public UInt32 Attributes;
+    //}
+
 
     internal class Kerner32
     {
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //public static extern IntPtr GetCurrentProcess();
+
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //public static extern bool CloseHandle(IntPtr hHandle);
+
+
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern void GetSystemTime(ref SYSTEMTIME _lpSystemTime);
 
@@ -44,7 +72,33 @@ namespace exw
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool SetSystemTime([In] ref SYSTEMTIME _lpSystemTime);
 
+        //[DllImport("kernel32.dll", EntryPoint = "GetLastError")]
+        //public static extern int GetLastError();
+
     }
+
+    //internal class AdvAPI32
+    //{
+    //    [DllImport("advapi32.dll", SetLastError = true)]
+    //    [return: MarshalAs(UnmanagedType.Bool)]
+    //    public static extern bool OpenProcessToken(IntPtr _processHandle, uint _desiredAccess, out IntPtr _tokenHandle);
+
+    //    [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    //    [return: MarshalAs(UnmanagedType.Bool)]
+    //    public static extern bool LookupPrivilegeValue(string _systemName, string _privilegeName, out LUID _privilegeLuid);
+
+    //    // Use this signature if you do not want the previous state
+    //    [DllImport("advapi32.dll", SetLastError = true)]
+    //    [return: MarshalAs(UnmanagedType.Bool)]
+    //    public static extern bool AdjustTokenPrivileges(IntPtr _tokenHandle,
+    //                                                    [MarshalAs(UnmanagedType.Bool)] bool _disableAllPrivileges,
+    //                                                    ref TOKEN_PRIVILEGES _newState,
+    //                                                    [In] uint _bufferLength,
+    //                                                    IntPtr _previousState,
+    //                                                    IntPtr _returnLength);
+
+    //}
+
 
 
 
